eb2c3ada35c7490a9e18af9195dc2b126717e44e
StrangeJaguar55 <124946085+StrangeJaguar55@users.noreply.github.com>
2023-03-22 13:29:08 -0400

    Create help.js

---
+++ Commands/Information/help.js
@@ -0,0 +1,8 @@
+const {MessageEmbed} = require('discord.js')
+
+const embed = new MessageEmbed()
+  .setTitle('Help Command')
+  .setDescription('Get help for StrangeBot')
+  .setImage('')
+
+//Allu can you add drop downs once we do more
