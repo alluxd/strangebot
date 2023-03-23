4f3ee71c23f3b031e6bb9cfe66a5033f92960bbb
AlluXD <aryananoop2020@gmail.com>
2023-03-23 11:22:53 +0530

    Updated ping.js

--- Commands/Information/ping.js
+++ Commands/Information/ping.js
@@ -1,12 +1,27 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
 data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Pings the bot.'),
 async execute(interaction, client) {
+
+    const uptime = process.uptime();
+const h = Math.floor(uptime / 3600);
+const m = Math.floor((uptime % 3600) / 60);
+const s = Math.floor(uptime % 60);
+
+    const pingEmbed = new EmbedBuilder()
+    .setTitle("Pong! 🏓")
+    .addFields(
+        { name: 'Latency 📶', value: `**${client.ws.ping}ms**` },
+        { name: 'Uptime ⌚', value: `**${h}h, ${m}m, ${s}s**` }
+    )
    await interaction.reply({
-    content: `Pong! ${client.ws.ping}ms! 🏓`
+    embeds: [
+        pingEmbed
+    ]
    })
     }
 };
